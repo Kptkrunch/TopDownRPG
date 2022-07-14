70a7a25489e2413c7ebdf8d45f146a23bc08b70f
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-07-14 16:12:32 -0500

    small amount of refactoring to the enemy AI scipt

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using Pathfinding;
+using UnityEngine.PlayerLoop;
+
+public class EnemyAI : MonoBehaviour
+{
+	public Transform target;
+
+	public float speed = 200f;
+	public float nextWaypointDistance = 3f;
+
+	public Transform enemyFacing;
+	
+
+	private Path _path;
+	private int _currentWaypoint = 0;
+	private bool _reachedEndOfPath = false;
+
+	private Seeker _seeker;
+	private Rigidbody2D _enemyRb2D;
+
+	private void Start()
+	{
+		_seeker = GetComponent<Seeker>();
+		_enemyRb2D = GetComponent<Rigidbody2D>();
+		
+		InvokeRepeating(nameof(UpdatePath), 0f, .5f);
+		_seeker.StartPath(_enemyRb2D.position, target.position, OnPathComplete);
+	}
+
+	void UpdatePath()
+	{
+		if (_seeker.IsDone())
+		{
+			_seeker.StartPath(_enemyRb2D.position, target.position, OnPathComplete);
+		}
+	}
+	void OnPathComplete(Path currentPath)
+	{
+		if (!currentPath.error)
+		{
+			_path = currentPath;
+			_currentWaypoint = 0;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if (_path == null)
+		{
+			return;
+		}
+
+		if (_currentWaypoint >= _path.vectorPath.Count)
+		{
+			_reachedEndOfPath = true;
+			return;
+		}
+		else
+		{
+			_reachedEndOfPath = false;
+		}
+
+		Vector2 direction = ((Vector2)_path.vectorPath[_currentWaypoint] - _enemyRb2D.position).normalized;
+		Vector2 force = direction * (speed * Time.deltaTime);
+		
+		_enemyRb2D.AddForce(force);
+
+		float distance = Vector2.Distance(_enemyRb2D.position, _path.vectorPath[_currentWaypoint]);
+
+		if (distance < nextWaypointDistance)
+		{
+			_currentWaypoint++;
+		}
+		
+		if (force.x >= 0.01f)
+		{
+			enemyFacing.localScale = new Vector3(-1f, 1f, 1f);
+		} else if (force.x <= -0.01f)
+		{
+			enemyFacing.localScale = new Vector3(1f, 1f, 1f);
+		}
+	}
+}
